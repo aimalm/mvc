ebd5b43dab2536557c5c6f84d5594acc7ca12d0b
Aimal-Maarij <51042919+Aimal-Maarij@users.noreply.github.com>
2021-02-01 05:14:10 +0100

    adding and testing the database connection

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 declare(strict_types=1);
 
+// Show errors so we get helpful information
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 //include all your model files here
+require_once './Model/config.php';
+require_once './Model/DatabaseManager.php';
 require 'Model/User.php';
 //include all your controllers here
 require 'Controller/HomepageController.php';
@@ -10,10 +17,29 @@ require 'Controller/InfoController.php';
 //you could write a simple IF here based on some $_GET or $_POST vars, to choose your controller
 //this file should never be more than 20 lines of code!
 
-$controller = new HomepageController();
+$databaseManager = new DatabaseManager($config['host'], $config['user'], $config['password'], $config['dbname']);
+$databaseManager->connect();
+$query = $databaseManager->connection->query("SELECT * FROM user");
+//var_dump($query);
+//$result = $query->fetchAll();
+foreach ($query as $id){
+    ?> 
+<h1> <?php echo $id["firstName"];
+ ?> </h1>
+<?php
+
+
+}
+
+
+
+$controller = new HomepageController($databaseManager);
 if(isset($_GET['page']) && $_GET['page'] === 'info') {
     $controller = new InfoController();
+
 }
 
 
 $controller->render($_GET, $_POST);
+//$controller->getId();
+?>
