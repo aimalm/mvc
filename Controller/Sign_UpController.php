5b3bff6c35be43a4126cf519797534a334d126c3
Aimal-Maarij <51042919+Aimal-Maarij@users.noreply.github.com>
2021-02-02 09:23:01 +0100

    WIP draft commit before a pull

--- Controller/Sign_UpController.php
+++ Controller/Sign_UpController.php
@@ -1,10 +1,11 @@
 <?php
-declare(strict_types = 1);
+
+declare(strict_types=1);
 
 class SignupController
 {
     public $databaseManager;
-   
+
     //render function with both $_GET and $_POST vars available if it would be needed.
     public function __construct(DatabaseManager $databaseManager)
     {
@@ -13,31 +14,41 @@ class SignupController
 
     public function render(array $GET, array $POST)
     {
-    
+
         //  $login=new LoginModel($databaseManager);
         //  $login->login();
 
-        if(isset($_POST['signup'])){
-            $name=$_POST['name'];
-            $lastName=$_POST['lastname'];
-            $email=$_POST['email'];
-            $password=$_POST['password'];
-            $profilePicture=$_POST['profilepic'];
-        
-        $sql="INSERT INTO user (`firstName`, `lastName`, `email`, `password`, `profilePicture`) VALUES ('$name', '$lastName','$email', '$password', '$profilePicture')";
-        $result=$this->databaseManager->connection->query($sql);
-        //$result->prepare($result);
-        //$result->execute([$name,$lastName,$email,$password,$profilePicture]);
+        if (isset($_POST['signup'])) {
+            $name = $_POST['name'];
+            $lastName = $_POST['lastname'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $profilePicture = $_POST['profilepic'];
 
-        header('Location: ./index.php?page=login');
-        //exit();
+            $sqlTocheckDuplicates = "SELECT * FROM user WHERE email ='$email'";
+            $result = $this->databaseManager->connection->query($sqlTocheckDuplicates);
+
+            foreach ($result as $user) {
+                //var_dump($em['email']);
+                if ($email === $user['email']) {
+                    echo "Already an account is made with this email";
+
+                    exit();
+                }
+            }
+            $sql = "INSERT INTO user (`firstName`, `lastName`, `email`, `password`, `profilePicture`) VALUES ('$name', '$lastName','$email', '$password', '$profilePicture')";
+            $result = $this->databaseManager->connection->query($sql);
+            //$result->prepare($result);
+            //$result->execute([$name,$lastName,$email,$password,$profilePicture]);
+
+            header('Location: ./index.php?page=login');
+            //exit();
+
         }
-        
 
-        
+
+
         //load the view
         require './View/sign_Up.php';
-        
     }
-    
 }
