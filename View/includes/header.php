599643eb3f92ef3c6d4fde4102e9aff21a364373
Valerie <akpotevalerie@gmail.com>
2021-02-03 11:11:36 +0100

    Merge manually

--- View/includes/header.php
+++ View/includes/header.php
@@ -24,18 +24,15 @@
                                 <li class="nav-item">
                                 <a class="nav-link" href="../../index.php">Log out</a>
                                 </li>
-<<<<<<< HEAD
                                 <li class="nav-item">
                                     <a class="nav-link" href="../../index.php?page=challenge">Challenge</a>
                                 </li>
-=======
                                 <li>
                                 <a class="nav-link" href="../../index.php?page=update">Update</a>
                                 <!-- http://localhost/becode-the-mountain/mvc/index.php?page=update -->
                                 </li>
                                 <a class="nav-link" href="../../index.php?page=edit">Edit</a>
 
->>>>>>> d3b244f2b2b863663bb469af621f73518d76d572
                                 <?php
                                 //exit();
                             }
